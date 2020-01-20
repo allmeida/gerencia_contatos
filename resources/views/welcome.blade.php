07ce78ca2e0028f2652fb651a459289020f97552
Francinildo Allmeida <allmeida.fran@gmail.com>
2020-01-20 09:10:30 -0400

    ajustando view welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,17 +81,12 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    AGENDA
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="/contatos">Cadastrar Contato</a>
+                    <a href="https://github.com/allmeida/gerencia_contatos.git">GitHub</a>
                 </div>
             </div>
         </div>
